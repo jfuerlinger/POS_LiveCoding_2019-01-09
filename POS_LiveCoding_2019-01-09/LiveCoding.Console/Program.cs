291459c1ade8c93171b8c2797886466c3af23657
Josef Fürlinger <josef.fuerlinger@gmail.com>
2019-01-09 21:33:57 +0100

    Enumeration sample added.

--- POS_LiveCoding_2019-01-09/LiveCoding.Console/Program.cs
+++ POS_LiveCoding_2019-01-09/LiveCoding.Console/Program.cs
@@ -6,10 +6,19 @@ namespace LiveCoding.Console
     {
         static void Main(string[] args)
         {
+            System.Console.ForegroundColor = ConsoleColor.DarkRed;
+
             System.Console.WriteLine($"Das Zeichen 7 ist eine Ziffer: {CharUtils.IsDigit('7')}." );
             System.Console.WriteLine($"Das Zeichen 7 ist eine Ziffer: {'7'.IsDigit()}.");
 
             System.Console.WriteLine($"Das Zeichen 'd' kommt in 'Donaudampfschifffahrtsgesellschaft' {"Donaudampfschifffahrtsgesellschaft".CountChars('d', ignoreCase: true)} mal vor");
+
+            // Beispiel Enumerationen 
+            Weekday dayOfTheWeek = Weekday.Sunday;
+            System.Console.WriteLine((int)dayOfTheWeek);
+
+            Weekday anotherDayOfTheWeek = Enum.Parse<Weekday>("Sunday");
+            System.Console.WriteLine(anotherDayOfTheWeek);
         }
     }
 }
